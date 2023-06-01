26fe8286cee5c7324ef58585a86ded207192bf19
Tanzim Mokammel <mtanzim@gmail.com>
2023-06-01 19:55:49 -0400

    pq

--- pq/pq.go
+++ pq/pq.go
@@ -1,19 +1,21 @@
 package pq
 
 import (
+	"errors"
 	"fmt"
+	"strings"
 
 	"golang.org/x/exp/constraints"
 )
 
 type PQ[T constraints.Ordered] struct {
-	keys    []T
+	keys    []*T
 	size    int
 	maxSize int
 }
 
 func NewMaxPQ[T constraints.Ordered](maxSize int) *PQ[T] {
-	keys := make([]T, maxSize+1)
+	keys := make([]*T, maxSize+1)
 	return &PQ[T]{keys: keys, size: 0, maxSize: maxSize}
 }
 
@@ -27,7 +29,7 @@ func (pq *PQ[T]) Size() int {
 
 func (pq *PQ[T]) Insert(key T) {
 	pq.size++
-	pq.keys[pq.size] = key
+	pq.keys[pq.size] = &key
 	pq.swim(pq.size)
 }
 
@@ -38,8 +40,37 @@ func (pq *PQ[T]) swim(k int) {
 	}
 }
 
+func (pq *PQ[T]) DelTop() (*T, error) {
+	if pq.IsEmpty() {
+		return nil, errors.New("pq is empty")
+	}
+	top := *pq.keys[1]
+	pq.exchange(1, pq.size)
+	pq.size--
+	pq.keys[pq.size+1] = nil
+	pq.sink(1)
+	return &top, nil
+}
+
+func (pq *PQ[T]) sink(k int) {
+	for 2*k <= pq.size {
+		leftChild := 2 * k
+		rightChild := leftChild + 1
+		chosenChild := leftChild
+		if leftChild < pq.size && pq.less(leftChild, rightChild) {
+			chosenChild = rightChild
+		}
+		if !pq.less(k, chosenChild) {
+			break
+		}
+		pq.exchange(k, chosenChild)
+		k = chosenChild
+	}
+
+}
+
 func (pq *PQ[T]) less(i, j int) bool {
-	return pq.keys[i] < pq.keys[j]
+	return *pq.keys[i] < *pq.keys[j]
 }
 
 func (pq *PQ[T]) exchange(i, j int) {
@@ -47,5 +78,11 @@ func (pq *PQ[T]) exchange(i, j int) {
 }
 
 func (pq *PQ[T]) String() string {
-	return fmt.Sprintf("%v", pq.keys)
+	var sb strings.Builder
+	for _, v := range pq.keys {
+		if v != nil {
+			sb.WriteString(fmt.Sprintf("%v ->", *v))
+		}
+	}
+	return sb.String()
 }
--- pq/pq_test.go
+++ pq/pq_test.go
@@ -1,8 +1,28 @@
 package pq
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func TestPQ(t *testing.T) {
+func TestPQ_Insert(t *testing.T) {
+
+	pq := NewMaxPQ[int](10)
+	pq.Insert(1)
+	pq.Insert(10)
+	pq.Insert(3)
+	pq.Insert(2)
+	pq.Insert(17)
+	// pq.Insert(17)
+
+	got := pq.String()
+	want := "17 ->10 ->3 ->1 ->2 ->"
+	if got != want {
+		t.Errorf("want = %s; got %s", want, got)
+	}
+}
+
+func TestPQ_DelMax(t *testing.T) {
 
 	pq := NewMaxPQ[int](10)
 	pq.Insert(1)
@@ -11,10 +31,18 @@ func TestPQ(t *testing.T) {
 	pq.Insert(2)
 	pq.Insert(17)
 
+	var err error
+	var v *int
+	vals := []int{}
+	for err == nil {
+		v, err = pq.DelTop()
+		if v != nil {
+			vals = append(vals, *v)
+		}
+	}
+	want := []int{17, 10, 3, 2, 1}
+	if !reflect.DeepEqual(vals, want) {
+		t.Errorf("want = %v; got %v", want, vals)
+	}
 
-    got := pq.String()
-		want := ""
-    if got != "" {
-        t.Errorf("Abs(-1) = %s; want %s", got, want)
-    }
 }
